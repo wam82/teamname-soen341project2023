7eac3746290c83477402942b7664c5bbb7b5e0ec
arcvelit <louka.f.s@gmail.com>
2023-02-28 17:25:34 -0500

    [create-database] Backend with rest API
    - Created api in node.js backend
    - Linked api with online database
    - Allow queries in query body to fetch user data

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from "./Header/Header";
 import './Header/Header.css';
--- server/server.js
+++ server/server.js
@@ -1,8 +1,47 @@
+const supabase = require('@supabase/supabase-js')
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
-    res.json({message: "Test" })
-})
+const database = supabase.createClient('https://woqpnszxdqmdgcyicfgc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvcXBuc3p4ZHFtZGdjeWljZmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc2MDI3MzAsImV4cCI6MTk5MzE3ODczMH0.6P8xHjbJ1B3S6fZRyP-p51_CoGK7SRGsJVugwxs2hB4')
 
-app.listen(5000, () => {console.log("Console started on port 5000")})
+async function fetchAllUsers(db)
+{
+    const {data, error} =  await database.from("users").select("*")
+    return data
+}
+
+async function fetchUsersByField(db, field, value)
+{
+    // field can be any of the user fields in a string
+    const {data, error} =  await database.from("users").select("*").eq(field, value)
+    return data
+}
+
+async function run()
+{
+
+    app.get("/api/select", async (req, res) => {
+
+        const query = req.query
+
+        //data = await fetchUsersByField(database, "username", query.username)
+        if (query["all"])
+        {
+            res.json({res: await fetchAllUsers(database)})
+        } else if (query["username"])
+        {
+            res.json({res: await fetchUsersByField(database, "username", query["username"])})
+        } else if (query["email"])
+        {
+            res.json({res: await fetchUsersByField(database, "email", query["email"])})
+        } else {
+            res.json({res: {}})
+        }
+
+    })
+        
+        app.listen(5000, () => {console.log("Console started on port 5000")})
+} 
+
+run()
+
